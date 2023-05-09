caca8c0e46359952aa83824ba53f7b2b66893910
Kozirskiy Ilya <ilya12koz@gmail.com>
2023-05-09 23:59:10 +0300

    mvvm

--- app/src/main/java/com/example/messenger/UsersActivity.java
+++ app/src/main/java/com/example/messenger/UsersActivity.java
@@ -4,32 +4,80 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class UsersActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerViewUsers;
+    private UsersAdapter usersAdapter;
+
     private UsersViewModel viewModel;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference("Messages");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String value = dataSnapshot.getValue(String.class);
+                    Log.d("UsersActivity", value);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        initViews();
         viewModel = new ViewModelProvider(this).get(UsersViewModel.class);
         observeViewModel();
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            User user = new User("id" + i, "name" + i, "LastName" + i, i, new Random().nextBoolean());
+            users.add(user);
+        }
+        usersAdapter.setUsers(users);
     }
 
-    private void observeViewModel(){
+    private void initViews() {
+        recyclerViewUsers = findViewById(R.id.recyclerViewUsers);
+        usersAdapter = new UsersAdapter();
+        recyclerViewUsers.setAdapter(usersAdapter);
+    }
+
+    private void observeViewModel() {
         viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
             @Override
             public void onChanged(FirebaseUser firebaseUser) {
-                if (firebaseUser == null){
+                if (firebaseUser == null) {
                     Intent intent = LoginActivity.newIntent(UsersActivity.this);
                     startActivity(intent);
                     finish();
@@ -47,13 +95,13 @@ public class UsersActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.item_logout){
+        if (item.getItemId() == R.id.item_logout) {
             viewModel.logout();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public static Intent newIntent(Context context){
-        return new Intent(context,UsersActivity.class);
+    public static Intent newIntent(Context context) {
+        return new Intent(context, UsersActivity.class);
     }
 }
